d8d6f2055f292b628dcf23ba073c4ca2899e881c
bayuik <bayuindrakusuma05@gmail.com>
2022-04-17 15:20:22 +0700

    add carform

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ require("dotenv").config();
 app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.get("/", (req, res)=> {
-    res.render("index");
-})
+app.get("/list-cars", (req, res) => {
+  res.render("index", { page: "List Cars" });
+});
+
+app.get("/add-car", (req, res) => {
+  res.render("carForm", { page: "Add New Car" });
+});
+
+app.get("/edit-car", (req, res) => {
+  res.render("carForm", { page: "Update Car Information" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
